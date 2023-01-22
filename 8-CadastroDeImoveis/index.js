51903c44770ca6e236ce17a4e57c4cf4cfff2afd
Igor Castilhos <igorcastilhos2010@hotmail.com>
2023-01-21 22:56:39 -0300

    Adding Exercicio 8

--- 8-CadastroDeImoveis/index.js
+++ 8-CadastroDeImoveis/index.js
@@ -2,39 +2,43 @@ let imoveis = [];
 let option = "";
 
 do {
-  let imovel = "";
-  for (let i = 0; i < imoveis.length; i++) {
-    imovel += i + 1 + "º - " + imoveis[i] + "\n";
-  }
-
-  option = prompt(
-    `Quantidade de imoveis: ${imoveis.length}
+  option = prompt(`    Bem-Vindo(a) ao Cadastro de Imóveis!
+    Quantidade de imoveis: ${imoveis.length}
+    Escolha uma opção:
     1. Salvar um novo imóvel
     2. Mostrar todos os imóveis
-    3. Sair`
-  );
+    3. Sair`);
 
   switch (option) {
     case "1":
-      const newImovel = prompt(
-        `Digite o nome da carta que você deseja adicionar`
-      );
-      imoveis.unshift(newImovel);
-      break;
+      const imovel = {};
 
-    case "2":
-      const pullCard = deck.shift();
-      if (!pullCard) {
-        alert(`Não há cartas no baralho!`);
-      } else {
-        alert(`Carta puxada: ${pullCard}`);
+      imovel.owner = prompt(`Digite o nome do proprietário: `);
+      imovel.bedrooms = prompt(`Digite a quantidade de quartos no imóvel: `);
+      imovel.bathrooms = prompt(`Digite a quantidade de banheiros no imóvel: `);
+      imovel.garage = prompt(`O imóvel possui garagem? (Sim/Não) `);
+
+      const confirmation = confirm(`Salvar este imóvel?
+      Proprietário: ${imovel.owner}
+      Quartos: ${imovel.bedrooms}
+      Banheiros: ${imovel.bathrooms}
+      Possui garagem? ${imovel.garage}`);
+      if (confirmation) {
+        imoveis.push(imovel);
       }
       break;
-
+    case "2":
+      for (let i = 0; i < imoveis.length; i++) {
+        alert(`Imóvel ${i + 1}
+        Proprietário: ${imoveis[i].owner}
+        Quartos: ${imoveis[i].bedrooms}
+        Banheiros: ${imoveis[i].bathrooms}
+        Possui garagem? ${imoveis[i].garage}`);
+      }
+      break;
     case "3":
       alert(`Saindo...`);
       break;
-
     default:
       alert(`Opção Inválida!`);
   }
