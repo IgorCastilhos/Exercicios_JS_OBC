b991a0b203a88d211112aa35f558a9b7bc4d45a0
Igor Castilhos <igorcastilhos2010@hotmail.com>
2023-02-08 23:32:08 -0300

    Adding 22,23,24

--- 22-ClassesParaUmBancoDigital/index.js
+++ 22-ClassesParaUmBancoDigital/index.js
@@ -1,1 +1,23 @@
- 
+const App = require("./App");
+
+App.createUser("igor@email.com", "Igor Castilhos");
+App.createUser("lucas@email.com", "Lucas Castilhos");
+App.createUser("juliana@email.com", "Juliana Castilhos");
+
+App.deposit("igor@email.com", 100);
+
+App.transfer("igor@email.com", "lucas@email.com", 20);
+
+App.changeLoanFee(10);
+
+App.takeLoan("juliana@email.com", 2000, 24);
+
+console.log(App.findUser("igor@email.com"));
+console.log(App.findUser("igor@email.com").account);
+
+console.log(App.findUser("lucas@email.com"));
+console.log(App.findUser("lucas@email.com").account);
+
+console.log(App.findUser("juliana@email.com"));
+console.log(App.findUser("juliana@email.com").account);
+console.log(App.findUser("juliana@email.com").account.loans[0].installments);
